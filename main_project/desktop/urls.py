ecb445d7fb2b10d99fc1d4808277ebedba09f4ac
“github-example-dvm” <strzelczykadrian151@gmail.com>
2023-01-02 22:57:29 +0100

    code refacor and new views look

--- main_project/desktop/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-from .views import BucketListView, TrashAddView
-
-urlpatterns = [
-    path('', BucketListView.as_view(), name = "home"),
-    path("add/", TrashAddView.as_view(), name = "add-trash"),
-    #path("facts/", name = "facts")
-]
