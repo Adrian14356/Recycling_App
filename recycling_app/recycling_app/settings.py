6214fd6d1cee008207a911c40cd53ac1a12c9ac9
“github-example-dvm” <strzelczykadrian151@gmail.com>
2023-02-27 21:10:02 +0100

    visual fixes

--- recycling_app/recycling_app/settings.py
+++ recycling_app/recycling_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from .env import DEBUG, SECRET
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)7o=_^$bj&u)p#6-ci%m9th-n1$%v!_mvjh3%2l4q3khj*of7^"
+SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
